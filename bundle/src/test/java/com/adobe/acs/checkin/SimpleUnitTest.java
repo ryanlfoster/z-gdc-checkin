1cce6f1450cabf886acd34a6d0bd1365da5247ed
upasanac <upasana.chaube@gmail.com>
2014-04-08 15:57:58 +0530

    Committing initial project base

---
+++ bundle/src/test/java/com/adobe/acs/checkin/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.adobe.acs.checkin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
